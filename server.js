58a5051cc1ace89ac2faa793b5fd8a06d420c313
calhounbryce13 <111155940+calhounbryce13@users.noreply.github.com>
2025-06-24 19:37:32 -0700

    server file origin update

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const PORT = 4000;
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors({
-    origin: 'http://127.0.0.1:5500'
+    origin: 'https://calhounbryce13.github.io'
 }));
 /////////////////////////////////////////////////////////////////
 
